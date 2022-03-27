7d3d07da97ccc7c2fd7848ee10f34625860d7737
beastboy241 <beastboy241@gmail.com>
2022-03-26 22:54:12 -0500

    reduce snake size

--- snake_game.py
+++ snake_game.py
@@ -109,7 +109,7 @@ class SnakeGame:
     for pt in self.snake:
       pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
       # making snake
-      pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+      pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+2, pt.y+2, 8, 8))
 
     pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
 
